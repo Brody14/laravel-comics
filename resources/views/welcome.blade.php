00e617c8414aab3f96f747238a23f7890322e436
Brody14 <stefania.spinas@gmail.com>
2023-04-07 11:06:49 +0200

    EDIT main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,50 @@
 
 @section('content')
 
+    <section class="hero-section">
+        <div class="hero">
+
+        </div>
+    </section>
+
+    <section class="main-content">
         <div class="container">
-           <div class="row g-3">
+            <span class="btn btn-blue badge">current series</span>
+            <div class="row g-3">
                
                {{-- @dump($comics) --}}
                @foreach($comics as $comic)
-                <div class="col-2">
+                <div class="col-2 br-card">
                     <img class="thumbs" src="{{ $comic['thumb']}}" alt="">
+                    <h2 class="title">{{ $comic['series'] }}</h2>
 
                 </div>
                 @endforeach
-           </div>
+            </div>
 
         </div>
+        <div class="container d-flex justify-content-center">
+            <button class="btn btn-blue">load more</button>
+        </div>
 
+    </section>
+
+    <section>
+        <div class="sidebar">
+            <div class="container d-flex justify-content-between">
+             @foreach($sidebarItems as $item)
+                <div class="sidebar_card d-flex align-items-center">
+                    <div class="sidebar_card-img">
+                        <img src="{{ $item['img'] }}" alt="">
+                    </div>
+                    <div class="sidebar_card-body">
+                    <p class="m-0">{{ $item['text'] }}</p>
+                    </div>
+                </div>
+            @endforeach
+            </div>
+        </div>
+    </section>
 
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -231,8 +231,31 @@ Route::get('/', function () {
                     "Joëlle Jones"
                 ],
             ]
-            ]
-        ];
+            ],
+            
+        'sidebarItems' => [
+            [
+                'img'=> 'buy-comics-digital-comics.png',
+                'text'=> 'digital comics',
+            ],
+            [
+                'img'=> 'buy-comics-merchandise.png',
+                'text'=> 'dc merchandise',
+            ],
+            [
+                'img'=> 'buy-comics-subscriptions.png',
+                'text'=> 'subscription',
+            ],
+            [
+                'img'=> 'buy-comics-shop-locator.png',
+                'text'=> 'comic shop locator',
+            ],
+            [
+                'img'=> 'buy-dc-power-visa.svg',
+                'text'=> 'dc power visa',
+            ],
+        ],
+    ];
           
     return view('welcome', $data);
 });
